c78dc326b563b7e0d9ef855b7a61c7f3da714ea6
Darosss <daroing12@gmail.com>
2023-07-26 02:37:16 +0200

    feat: add AuthContextProvider + DefaultWrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import { Home } from "./home";
 import { HeadMenu, CategoriesMenu } from "./menu";
 import { Footer } from "./footer";
+import { AuthForms } from "./authForms";
+import { AuthContextProvider } from "./auth";
 
 export function App() {
+  return (
+    <>
+      <AuthContextProvider>
+        <Router>
+          <Routes>
+            <Route element={<DefaultWrapper />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+            <Route path="/login" element={<AuthForms />} />
+          </Routes>
+        </Router>
+      </AuthContextProvider>
+    </>
+  );
+}
+
+function DefaultWrapper() {
   return (
     <>
       <div className="sticky top-0 z-50 bg-body">
@@ -15,12 +40,7 @@ export function App() {
       </div>
       <main className="bg-content">
         <div className="container-fluid">
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/xd" element={<>xd</>} />
-            </Routes>
-          </Router>
+          <Outlet />
         </div>
         <Footer />
       </main>
