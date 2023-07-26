56596861d775c3bea90dd69e94258f0bf6172d35
Darosss <daroing12@gmail.com>
2023-07-26 22:43:26 +0200

    feat: add proper links to Menu

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import { SearchBar } from "../searchBar";
+import { MenuImages, MenuRoutes } from "./menu.enum";
+import { AuthContext } from "../auth";
+import { ProfilDropdown } from "./ProfilDropdown";
 
 export function HeadMenu() {
+  const { user } = useContext(AuthContext);
+
   return (
     <>
       <div>
@@ -17,12 +23,42 @@ export function HeadMenu() {
                   <SearchBar />
                 </div>
                 <div className="max-w-[680px] pl-[10px]">
-                  <div className="flex items-center justify-between [&>div]:mr-[10px] [&>div]:p-[10px] [&>div]:cursor-pointer">
-                    <div>kod pocztowy</div>
-                    <div>kontakt</div>
-                    <div> listy zakupowe</div>
-                    <div> Zaloguj</div>
-                    <div> koszyk</div>
+                  <div className="flex items-center justify-between [&>a]:flex [&>a]:items-center gap-2 [&>*>img]:m-2 [&>*>img]:w-[30px] [&>div]:flex">
+                    <a href={MenuRoutes.ZIP_CODE} className="hover:underline">
+                      <img src={MenuImages.ZIP_CODE} />
+                      Kod pocztowy
+                    </a>
+                    <a href={MenuRoutes.CONTACT} className="hover:underline">
+                      <img src={MenuImages.CONTACT} />
+                      Kontakt
+                    </a>
+                    <a
+                      href={user ? MenuRoutes.SHOP_LIST : MenuRoutes.LOGIN}
+                      className="hover:underline"
+                    >
+                      <img src={MenuImages.SHOP_LIST} />
+                      Listy zakupowe
+                    </a>
+
+                    {user ? (
+                      <div className="hover:underline [&>*]:text-default [&>*]:border-0">
+                        <img src={MenuImages.LOGIN} />
+                        <ProfilDropdown user={user} />
+                      </div>
+                    ) : (
+                      <a href={MenuRoutes.LOGIN} className="hover:underline">
+                        <img src={MenuImages.LOGIN} />
+                        Zaloguj się <br /> Załóż konto
+                      </a>
+                    )}
+
+                    <a href={MenuRoutes.CART} className="hover:underline">
+                      <img src={MenuImages.CART} />
+                      0,00 zł
+                      {/* TODO: add get price from localstorage
+                        // probably will need to store items ids and price into localstorage
+                      */}
+                    </a>
                   </div>
                 </div>
               </div>
