362bf275e1f0d8e738e6c70ea2a9995e805cd1a3
Darosss <daroing12@gmail.com>
2023-07-26 22:42:14 +0200

    feat: add MenuRoutes + MenuImages enums

---
+++ src/menu/menu.enum.ts
@@ -0,0 +1,16 @@
+export enum MenuRoutes {
+  ZIP_CODE = "/soon",
+  CONTACT = "/soon/2",
+  CART = "/koszyk",
+  LOGIN = "/login",
+  SHOP_LIST = "/inventory/list",
+  PROFIL = "/profil",
+}
+
+export enum MenuImages {
+  ZIP_CODE = "icons/pin-empty.svg",
+  CONTACT = "icons/contact-empty.svg",
+  CART = "icons/cart-empty.svg",
+  LOGIN = "icons/login-empty.svg",
+  SHOP_LIST = "icons/shop-list-empty.svg",
+}
