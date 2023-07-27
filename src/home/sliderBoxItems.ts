8636f9bc67aa50a708642e9257345ba256c355ba
Darosss <daroing12@gmail.com>
2023-07-27 17:02:53 +0200

    feat: add sliderBoxItems

---
+++ src/home/sliderBoxItems.ts
@@ -0,0 +1,58 @@
+import { SliderBoxInfoItems } from "../interfaces/components";
+
+export const homeSliderBoxInfoItems: SliderBoxInfoItems[] = [
+  {
+    name: "Łatwe i wygodne zakupy online. ",
+    nameDesc: "Jak pestka.",
+    description:
+      "Ułatwiamy Wam życie: zakupy online stają się prostsze, szybsze i tańsze. Przyjemniejsze, niż w każdym innym sklepie.",
+    imgSrc: "/assets/widgets/home/icon-online.svg",
+  },
+  {
+    name: "Konkurencyjne ceny. Bez ściemy. ",
+    nameDesc: "I z rymem.",
+    description:
+      "Zamiast w kółko śpiewać o cudownych promocjach i włączaniu niskich cen - po prostu je mamy. Zawsze.",
+    imgSrc: "/assets/widgets/home/icon-price.svg",
+    url: "/alarmcenowy",
+  },
+  {
+    name: "2 mln produktów w ofercie. ",
+    nameDesc: "Znajdziesz u nas wszystko.",
+    description:
+      "Oszczędzamy Wam długich poszukiwań i obaw o zawyżone ceny, jakość produktów, czy czas dostawy. Zamiast szukać, wpadnij do nas.",
+    imgSrc: "/assets/widgets/home/icon-products.svg",
+  },
+  {
+    name: "Ponad 3 miliony klientów. ",
+    nameDesc: "nie może się mylić.",
+    description:
+      "Budujemy pozytywne doświadczenia i stawiamy na rozwój zakupów pozbawionych wad oraz problemów, z którymi stykacie się w innych miejscach.",
+    imgSrc: "/assets/widgets/home/icon-clients.svg",
+    url: "/onas",
+  },
+  {
+    name: "Szybka dostawa",
+    nameDesc: "w wielu opcjach.",
+    description:
+      "Od paczkomatów, przez kuriera po 15 000 miejsc odbioru osobistego. Na drony też przyjdzie czas.",
+    imgSrc: "/assets/widgets/home/icon-delivery.svg",
+    url: "/sposoby-dostawy-i-platnosci",
+  },
+  {
+    name: "Obsługa klientów",
+    nameDesc: "na pierwszym planie.",
+    description:
+      "Ogarniamy standardy, w których sami chcielibyśmy kupować. Nieustannie i za każdym razem jeszcze lepiej.",
+    imgSrc: "/assets/widgets/home/icon-service.svg",
+    url: "/onas",
+  },
+  {
+    name: "Pomagamy",
+    nameDesc: "jak doba długa i szeroka.",
+    description:
+      "Napisz. Zadzwoń. Wpadnij na social media. Porozmawiajmy jeśli czegoś potrzebujesz. Doradzimy i pomożemy najlepiej jak potrafimy.",
+    imgSrc: "/assets/widgets/home/icon-help-center.svg",
+    url: "/soon",
+  },
+];
