7718550ed078b8bf0e0c8f686f14d28913bc3414
Darosss <daroing12@gmail.com>
2023-07-27 20:31:34 +0200

    feat: move cat menu into separate folder + make dropdown

--- src/menu/CategoriesMenu.tsx
+++ src/menu/CategoriesMenu.tsx
@@ -7,26 +7,7 @@ export function CategoriesMenu() {
       <div className="min-w-[120px] bg-white shadow-default">
         <div className="container-fluid">
           <div className="mr-[8px] flex flex-wrap relative">
-            <nav className="max-w-full w-[260px] shadow-default flex content-center justify-center ">
-              <div className="flex align-center items-center text-orange font-semibold ">
-                KATEGORIE
-                {/* cn-heading */}
-              </div>
-              <div className="absolute top-full rounded-b-xl min-h-[597px] left-0 right-0 hover:z-40">
-                {/* cn-window*/}
-                <div className="bg-body w-[260px] min-h-[597px] ">
-                  {/* cn-bar */}
-                  <div>
-                    {/* cn-shop cn current frontend */}
-                    <ul className="m-[-10px_0_10px] p-[16px_0_8px] [&>li]:p-1 text-left [&>li]:font-semibold text-[14px] [&>li>span]:p-[8px_40px_8px_16px] [&>li]:mt-[8px] ">
-                      {/* categories
-                      cn current deparments cn level */}
-                      <ParentCategoryDropdown />
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </nav>
+            <ParentCategoryDropdown />
             <nav className="w-100 min-w-[400px] flex-[1_1_calc(100%-280px)] w-[calc(100%-280%)]">
               <div className="pl-[10px] w-full justify-between items-end ">
                 <div className="flex [&>div]:mr-[10px] [&>div]:p-[10px] [&>div]:cursor-pointer justify-between">
--- src/menu/ParentCategoryDropdown.tsx
+++ src/menu/ParentCategoryDropdown.tsx
@@ -3,29 +3,54 @@ import { useContext } from "react";
 import { CategoryMenu } from "./CategoryMenu";
 import { CategoriesContext } from "../api/CategoriesContext";
 import { replaceWholeSpaces } from "../helpers/string.helpers";
+import { useLocation } from "react-router-dom";
 
 export function ParentCategoryDropdown() {
   const { categoriesList } = useContext(CategoriesContext);
+  const { pathname } = useLocation();
 
   return (
     <>
-      {categoriesList.map((category, idx) => (
-        <li key={idx} className="group">
-          <a
-            href={`/${replaceWholeSpaces(category.name)}/`}
-            className="group-hover:text-orange"
-          >
-            <span className=" p-[8px_40px_8px_16px] relative z-30">
-              {category.name}
-            </span>
-          </a>
+      <nav className="group/menu head-menu max-w-full w-[260px] shadow-default flex content-center justify-center ">
+        <div className="flex align-center items-center text-orange font-semibold">
+          KATEGORIE
+          {/* cn-heading */}
+        </div>
+        <div
+          className={`${
+            pathname === "/" ? "flex" : "group-hover/menu:flex hidden"
+          } absolute top-full rounded-b-xl min-h-[597px] left-0 right-0 hover:z-40`}
+        >
+          {/* cn-window*/}
+          <div className="bg-body w-[260px] min-h-[597px] ">
+            {/* cn-bar */}
+            <div>
+              {/* cn-shop cn current frontend */}
+              <ul className="m-[-10px_0_10px] p-[16px_0_8px] [&>li]:p-1 text-left [&>li]:font-semibold text-[14px] [&>li>span]:p-[8px_40px_8px_16px] [&>li]:mt-[8px] ">
+                {/* categories
+            cn current deparments cn level */}
+                {categoriesList.map((category, idx) => (
+                  <li key={idx} className="group">
+                    <a
+                      href={`/${replaceWholeSpaces(category.name)}/`}
+                      className="group-hover:text-orange"
+                    >
+                      <span className=" p-[8px_40px_8px_16px] relative z-30">
+                        {category.name}
+                      </span>
+                    </a>
 
-          <CategoryMenu
-            className="group-hover:block"
-            subcategories={category.subcategories}
-          />
-        </li>
-      ))}
+                    <CategoryMenu
+                      className="group-hover:block"
+                      subcategories={category.subcategories}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
