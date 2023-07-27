7c104add3de179e61303fb2c3444d85c1624940d
Darosss <daroing12@gmail.com>
2023-07-27 17:03:19 +0200

    refactor: replace hard coded items with homeSliderBoxInfoItems

--- src/home/HomeSections.tsx
+++ src/home/HomeSections.tsx
@@ -1,6 +1,7 @@
 import { SliderBox } from "../sliderBox";
 import { SliderBoxInfo } from "../sliderBoxInfo";
 import { PromotionCard } from "./PromotionCard";
+import { homeSliderBoxInfoItems } from "./sliderBoxItems";
 
 export function HomeSections() {
   return (
@@ -9,18 +10,7 @@ export function HomeSections() {
         {/* home seciton row  */}
         <div className="w-full">
           {/* about company slider */}
-          <SliderBoxInfo
-            title="Odkryj morele"
-            items={[
-              {
-                name: "Łatwe i wygodne zakupy online. ",
-                nameDesc: "Jak pestka.",
-                description:
-                  "Ułatwiamy Wam życie: zakupy online stają się prostsze, szybsze i tańsze. Przyjemniejsze, niż w każdym innym sklepie.",
-                imgSrc: "/assets/widgets/home/icon-online.svg",
-              },
-            ]}
-          />
+          <SliderBoxInfo title="Odkryj morele" items={homeSliderBoxInfoItems} />
         </div>
         <div className="w-[400px] rounded-[8px] overflow-hidden">
           {/* home section promotions */}
