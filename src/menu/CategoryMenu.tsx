9c793098bbc3227732337d336212466c7a994488
Darosss <daroing12@gmail.com>
2023-07-27 12:36:04 +0200

    feat: add show 3rd depth categories in menus

--- src/menu/CategoryMenu.tsx
+++ src/menu/CategoryMenu.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Category } from "../interfaces";
+import { replaceWholeSpaces } from "../helpers/string.helpers";
 
 export function CategoryMenu({
   subcategories,
@@ -11,14 +12,26 @@ export function CategoryMenu({
   return (
     <>
       <div
-        className={`hidden rounded border-[2px] border-orange bg-body absolute left-[260px] min-h-[100%] p-[32px] top-0 z-1 right-0 ${className}`}
+        className={`hidden  rounded border-[2px] border-orange bg-body absolute left-[260px] min-h-[100%] p-[32px] top-0 z-1 right-0 ${className} `}
       >
         {/* cn-shop-window cn-level */}
         <div className="flex justify-between">
           {/* cn rows */}
+
           {subcategories?.map((category, idx) => (
-            <ul key={idx}>
-              <li>{category.name}</li>
+            <ul key={idx} className="font-normal">
+              <li className="font-semibold">
+                <a href={`/${replaceWholeSpaces(category.name)}`}>
+                  {category.name}
+                </a>
+              </li>
+              {category.subcategories?.map((thirdSubCat, thidIdx) => (
+                <li key={thidIdx}>
+                  <a href={`/${replaceWholeSpaces(thirdSubCat.name)}`}>
+                    {thirdSubCat.name}
+                  </a>
+                </li>
+              ))}
             </ul>
           ))}
         </div>
