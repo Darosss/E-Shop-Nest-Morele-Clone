cdb63ea7fc9a964f40f54bd14d4693d6e8f4c504
Darosss <daroing12@gmail.com>
2023-08-12 11:26:09 +0200

    refactor: move subcategories into separate component

---
+++ src/categories/categoryHeadView/AllCategories.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { CategoriesEndpoints } from "../../api/backend-endpoints";
+import { Category } from "../../interfaces";
+
+export function AllCategories() {
+  const { categoryParent, categorySubParent } = useParams();
+  const [subCategories, setSubCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    if (categoryParent && categorySubParent) {
+      axios
+        .get(
+          `${CategoriesEndpoints.GET_CATEGORY_BY_SLUG}/${categoryParent}/${categorySubParent}`
+        )
+        .then((response) => {
+          const responseData = response.data.data;
+          if (!responseData) return;
+          setSubCategories(responseData.subcategories);
+        });
+    }
+  }, [categoryParent, categorySubParent]);
+
+  return (
+    <>
+      {subCategories?.map((category, idx) => (
+        <Link
+          key={idx}
+          to={`/category/${category.name}/${category.id}`}
+          className="flex flex-[0_0_24%] items-center justify-between p-2"
+        >
+          <div className="h-[80px]  overflow-hidden flex">
+            <img src={`/category/${category.images[0]}`} alt="category" />
+          </div>
+          <div className="text-[16px] p-2 flex flex-[0_0_calc(100%-104px)]">
+            {category.name} (400)
+            {/* TODO: add count of items in category */}
+          </div>
+        </Link>
+      ))}
+    </>
+  );
+}
--- src/categories/categoryHeadView/CategoryHeadView.tsx
+++ src/categories/categoryHeadView/CategoryHeadView.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { CategoryBreadcrumbs } from "../Breadcrumbs";
+import { AllCategories } from "./AllCategories";
 
 export function CategoryHeadView() {
   const { state } = useLocation();
@@ -24,25 +25,7 @@ export function CategoryHeadView() {
           Wszystkie categorie
         </div>
         <div className="flex flex-wrap [&>a]:m-2 [&>a]:m-2 p-2">
-          {Array(10)
-            .fill(0)
-            .map((_, idx) => (
-              <Link
-                key={idx}
-                to="/category/test"
-                className="flex flex-[0_0_24%] items-center justify-between p-2"
-              >
-                <div className="h-[80px]  overflow-hidden flex">
-                  <img
-                    src="/category/development-category.jpg"
-                    alt="category"
-                  />
-                </div>
-                <div className="text-[16px] p-2 flex flex-[0_0_calc(100%-104px)]">
-                  Mikrofony (500)
-                </div>
-              </Link>
-            ))}
+          <AllCategories  />
         </div>
       </div>
 
