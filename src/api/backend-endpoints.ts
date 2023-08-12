b9026ec0e34cfd12cf99851dd685ea73d85fc707
Darosss <daroing12@gmail.com>
2023-08-12 11:31:59 +0200

    feat: add category slug +auth profile enums

--- src/api/backend-endpoints.ts
+++ src/api/backend-endpoints.ts
@@ -1,8 +1,10 @@
 export enum CategoriesEndpoints {
   GET_HEAD_CATEGORIES = "/category",
+  GET_CATEGORY_BY_SLUG = GET_HEAD_CATEGORIES + "/slug",
 }
 
 export enum AuthEndpoints {
   LOGIN = "/auth/login",
   REGISTER = "/auth/register",
+  PROFILE = "/auth/profile",
 }
