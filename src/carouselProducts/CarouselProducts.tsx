a68820218bdf2e6df1624164c7b659b8ea5d24df
Darosss <daroing12@gmail.com>
2023-08-15 14:18:08 +0200

    feat: add mostBought option with MostBought component

--- src/carouselProducts/CarouselProducts.tsx
+++ src/carouselProducts/CarouselProducts.tsx
@@ -5,11 +5,13 @@ import { Carousel } from "flowbite-react";
 interface CarouselProductsProps {
   items: Product[];
   recomended?: boolean;
+  mostBought?: boolean;
 }
 
 export function CarouselProducts({
   items,
   recomended = false,
+  mostBought = false,
 }: CarouselProductsProps) {
   return (
     <div className="w-full h-[350px]">
@@ -28,8 +30,16 @@ export function CarouselProducts({
               to={"url" + idx}
               className=" inline-block max-w-[168px] w-[168px] p-[0_8px_8px] mr-[42px] [&>div]:mt-[4px]"
             >
+              {mostBought ? (
+                <div className="flex border border-orange rounded-full items-center [&>div]:text-[12px]">
+                  <div className="rounded-full m-[2px] mr-2 bg-orange w-[20px]  flex justify-center">
+                    {idx + 1}
+                  </div>
+                  <div>Kupiono: {item.sold} sztuk</div>
+                </div>
+              ) : null}
               <div>
-                <img src="assets/12775308_0_i256.jpg" />
+                <img src="/assets/12775308_0_i256.jpg" />
               </div>
               <div className="mb-[12px] mt-[10px] min-h-[20px]">
                 {/* ps price box */}
