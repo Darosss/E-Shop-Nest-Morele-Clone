d286860f88da2fbd6bc642ca24517bc08b13df2e
Darosss <daroing12@gmail.com>
2023-09-24 13:33:57 +0200

    refactor: move menu items into separate component, improve styles

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -1,67 +1,31 @@
-import { useContext } from "react";
 import { SearchBar } from "../searchBar";
-import { MenuImages, MenuRoutes } from "./menu.enum";
-import { AuthContext } from "../auth";
-import { ProfilDropdown } from "./ProfilDropdown";
 import { Link } from "react-router-dom";
+import { MenuHeadButtons } from "./MenuHeadButtons";
 
 export function HeadMenu() {
-  const { user } = useContext(AuthContext);
-
   return (
     <>
       <div>
         <header className="min-w-[120px] py-[20px] bg-white shadow-default">
           <div className="container-fluid">
             <div className="ml-[-8px] mr-[8px] flex flex-row flex-wrap">
-              <div className="max-w-full w-[280px] items-center flex pt-[2px] pl-[42px]">
+              <div className="max-w-full items-center flex pt-[2px] md:mr-1 2xl:pl-[42px] w-[120px] md:w-[150px] xl:w-[250px] 2xl:mr-0 ">
                 <h1>
                   <Link to="/">
-                    <img src="/static/shop/logo/image-logo-morele.svg" />
+                    <img
+                      src="/static/shop/logo/image-logo-morele.svg"
+                      className="w-[90%]"
+                    />
                   </Link>
                 </h1>
               </div>
               <div className="flex-[1_1_calc(100%-280px)] w-[calc(100%-280%)] flex items-center">
-                <div className="w-[50%] flex-[0_0_50%]">
+                <div className="flex-[0_0_100%] lg:flex-[0_0_50%] xl:flex-[0_0_40%] 2xl:flex-[0_0_50%]">
                   <SearchBar />
                 </div>
-                <div className="max-w-[680px] pl-[10px]">
+                <div className="max-w-[680px] pl-[10px] relative hidden lg:flex">
                   <div className="flex items-center justify-between [&>a]:flex [&>a]:items-center gap-2 [&>*>img]:m-2 [&>*>img]:w-[30px] [&>div]:flex">
-                    <a href={MenuRoutes.ZIP_CODE} className="hover:underline">
-                      <img src={MenuImages.ZIP_CODE} />
-                      Kod pocztowy
-                    </a>
-                    <a href={MenuRoutes.CONTACT} className="hover:underline">
-                      <img src={MenuImages.CONTACT} />
-                      Kontakt
-                    </a>
-                    <a
-                      href={user ? MenuRoutes.SHOP_LIST : MenuRoutes.LOGIN}
-                      className="hover:underline"
-                    >
-                      <img src={MenuImages.SHOP_LIST} />
-                      Listy zakupowe
-                    </a>
-
-                    {user ? (
-                      <div className="hover:underline [&>*]:text-default [&>*]:border-0">
-                        <img src={MenuImages.LOGIN} />
-                        <ProfilDropdown user={user} />
-                      </div>
-                    ) : (
-                      <a href={MenuRoutes.LOGIN} className="hover:underline">
-                        <img src={MenuImages.LOGIN} />
-                        Zaloguj się <br /> Załóż konto
-                      </a>
-                    )}
-
-                    <a href={MenuRoutes.CART} className="hover:underline">
-                      <img src={MenuImages.CART} />
-                      0,00 zł
-                      {/* TODO: add get price from localstorage
-                        // probably will need to store items ids and price into localstorage
-                      */}
-                    </a>
+                    <MenuHeadButtons />
                   </div>
                 </div>
               </div>
